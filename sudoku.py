00352347b378c908db220f459ad50a572db21242
Cher Huang <huang.cher810@gmail.com>
2024-04-24 14:03:03 -0400

    updated all screens

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,5 @@
 import pygame
 from board import Board
-from cell import Cell
-
 
 pygame.init()
 screen = pygame.display.set_mode((540, 600))
@@ -10,7 +8,6 @@ screen.fill((205, 173, 135))
 
 def main():
     draw_game_start()
-    boardobj = Board(540, 600, screen, None)
     
     while True:
         for event in pygame.event.get():
@@ -31,73 +28,88 @@ def main():
                         case pygame.K_o:
                             draw_game_over()
 
-                        case pygame.K_r:
-                            draw_game_run()
-
                         case pygame.K_b:
-                            boardobj.draw()
+                            draw_game_run()
 
         pygame.display.update()
 
 def draw_game_start():
-    #screen.blit(pygame.image.load("bg.jpg"), (0, 0))
-    image = pygame.image.load("bg.jpg")
-    image = pygame.transform.scale(image, (540, 600))
-    screen.blit(image, (0, 0))
-    welcome_surf = pygame.font.Font("font.ttf", 75).render("Welcome to Sudoku", 1, "black")
-    welcome_rect = welcome_surf.get_rect(center=(270, 115))
+    # centers and imports the sudoku logo
+    screen.blit(pygame.image.load("bg.jpg"), (0, 0))
+
+    # prints and centers the welcome text using the correct font, font size, color
+    welcome_surf = pygame.font.Font("font.ttf", 50).render("Welcome to Sudoku", 1, "black")
+    welcome_rect = welcome_surf.get_rect(center=(270, 80))
     screen.blit(welcome_surf, welcome_rect)
 
-    mode_surf = pygame.font.Font("font.ttf", 50).render("Select Game Mode:", 1, "black")
-    mode_rect = mode_surf.get_rect(center=(270, 730))
+    # prints and centers the game mode selection text using the correct font, font size, color
+    mode_surf = pygame.font.Font("font.ttf", 35).render("Select Game Mode:", 1, "black")
+    mode_rect = mode_surf.get_rect(center=(270, 500))
     screen.blit(mode_surf, mode_rect)
 
-    pygame.draw.rect(screen, "white", pygame.Rect(250, 780, 100, 50))
-    easy_surf = pygame.font.Font("font.ttf", 22).render("Easy", 1, "black")
-    easy_rect = easy_surf.get_rect(center=(300, 805))
+    # prints and centers a rectangle
+    pygame.draw.rect(screen, "white", pygame.Rect(75, 530, 90, 35))
+    # prints and centers the game mode: easy text using the correct font, font size, color
+    easy_surf = pygame.font.Font("font.ttf", 18).render("Easy", 1, "black")
+    easy_rect = easy_surf.get_rect(center=(120, 548))
     screen.blit(easy_surf, easy_rect)
 
-    pygame.draw.rect(screen, "white", pygame.Rect(400, 780, 100, 50))
-    medium_surf = pygame.font.Font("font.ttf", 22).render("Medium", 1, "black")
-    medium_rect = medium_surf.get_rect(center=(270, 805))
+    # prints and centers a rectangle
+    pygame.draw.rect(screen, "white", pygame.Rect(225, 530, 90, 35))
+    # prints and centers the game mode: medium text using the correct font, font size, color
+    medium_surf = pygame.font.Font("font.ttf", 18).render("Medium", 1, "black")
+    medium_rect = medium_surf.get_rect(center=(270, 548))
     screen.blit(medium_surf, medium_rect)
 
-    pygame.draw.rect(screen, "white", pygame.Rect(550, 780, 100, 50))
-    hard_surf = pygame.font.Font("font.ttf", 22).render("Hard", 1, "black")
-    hard_rect = hard_surf.get_rect(center=(600, 805))
+    # prints and centers a rectangle
+    pygame.draw.rect(screen, "white", pygame.Rect(375, 530, 90, 35))
+    # prints and centers the game mode: hard text using the correct font, font size, color
+    hard_surf = pygame.font.Font("font.ttf", 18).render("Hard", 1, "black")
+    hard_rect = hard_surf.get_rect(center=(420, 548))
     screen.blit(hard_surf, hard_rect)
 
 def draw_game_won():
-    screen.fill((205, 173, 135))
-    screen.blit(pygame.image.load("bg.jpg"), (50, 50))
+    # centers and imports the sudoku logo
+    screen.blit(pygame.image.load("bg.jpg"), (0, 0))
 
-    won_surf = pygame.font.Font("font.ttf", 75).render("Game Won!", 1, "black")
-    won_rect = won_surf.get_rect(center=(450, 115))
+    # prints and centers the winner text using the correct font, font size, color
+    won_surf = pygame.font.Font("font.ttf", 50).render("Game Won!", 1, "black")
+    won_rect = won_surf.get_rect(center=(270, 80))
     screen.blit(won_surf, won_rect)
 
-    pygame.draw.rect(screen, "white", pygame.Rect(400, 780, 100, 50))
-    exit_surf = pygame.font.Font("font.ttf", 22).render("Exit", 1, "black")
-    exit_rect = exit_surf.get_rect(center=(450, 805))
+    # prints and centers a rectangle
+    pygame.draw.rect(screen, "white", pygame.Rect(225, 530, 90, 35))
+    # prints and centers the exit text using the correct font, font size, color
+    exit_surf = pygame.font.Font("font.ttf", 18).render("Exit", 1, "black")
+    exit_rect = exit_surf.get_rect(center=(270, 548))
     screen.blit(exit_surf, exit_rect)
 
 def draw_game_over():
-    screen.fill((205, 173, 135))
-    screen.blit(pygame.image.load("bg.jpg"), (50, 50))
+    # centers and imports the sudoku logo
+    screen.blit(pygame.image.load("bg.jpg"), (0, 0))
 
-    welcome_surf = pygame.font.Font("font.ttf", 75).render("Game Over :(", 1, "black")
-    welcome_rect = welcome_surf.get_rect(center=(450, 115))
+    # prints and centers the losing text using the correct font, font size, color
+    welcome_surf = pygame.font.Font("font.ttf", 50).render("Game Over :(", 1, "black")
+    welcome_rect = welcome_surf.get_rect(center=(270, 80))
     screen.blit(welcome_surf, welcome_rect)
 
-    pygame.draw.rect(screen, "white", pygame.Rect(400, 780, 100, 50))
-    restart_surf = pygame.font.Font("font.ttf", 22).render("Restart", 1, "black")
-    restart_rect = restart_surf.get_rect(center=(450, 805))
+    # prints and centers a rectangle
+    pygame.draw.rect(screen, "white", pygame.Rect(225, 530, 90, 35))
+    # prints and centers the restart text using the correct font, font size, color
+    restart_surf = pygame.font.Font("font.ttf", 18).render("Restart", 1, "black")
+    restart_rect = restart_surf.get_rect(center=(270, 548))
     screen.blit(restart_surf, restart_rect)
 
 def draw_game_run():
     screen.fill((205, 173, 135))
-    c = Cell(1,0,0, screen)
-    c.draw()
+    Board(540, 600, screen, None).draw()
 
+    # prints and centers a rectangle
+    pygame.draw.rect(screen, "white", pygame.Rect(225, 530, 90, 35))
+    # prints and centers the restart text using the correct font, font size, color
+    restart_surf = pygame.font.Font("font.ttf", 22).render("Restart", 1, "black")
+    restart_rect = restart_surf.get_rect(center=(450, 548))
+    screen.blit(restart_surf, restart_rect)
 
 if __name__ == "__main__":
     main()
